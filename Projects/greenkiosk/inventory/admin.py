f121f372a2a29754a13d025a68a51b3ad10263ca
lekishonsheila <lekishonsheila@gmail.com>
2023-07-05 23:36:05 +0300

    all python classwork

--- Projects/greenkiosk/inventory/admin.py
+++ Projects/greenkiosk/inventory/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from .models import Product
 
 # Register your models here.
+from .models import Product
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ("name","stock","price","date_created")
+    list_display = ("name","stock","price","date_created","description","date_updated","image")
 
 admin.site.register(Product,ProductAdmin)
