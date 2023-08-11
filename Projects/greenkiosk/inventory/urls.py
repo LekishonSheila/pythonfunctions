b1081f0286143b089086e9469bfcea0db0bea0ab
Lekishon <lekishonsheila@gmail.com>
2023-08-11 21:33:08 +0300

    added more features

--- Projects/greenkiosk/inventory/urls.py
+++ Projects/greenkiosk/inventory/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path 
 from .views import product_upload_view
+from .views import products_list
+from .views import product_detail_view
+
 
 urlpatterns = [
     path("products/upload/", product_upload_view, name="product_uploadview"),
+    path("products/list",products_list,name="products_list_view"),
+    path("products/<int:id>/",product_detail_view,name="products_detail"),
+   
 ]
--- Projects/greenkiosk/inventory/views.py
+++ Projects/greenkiosk/inventory/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
 from .forms import ProductUploadForm
+fromb .models import Product
 
 # Create your views here.
  
 def product_upload_view(request):
     form = ProductUploadForm()
     return render(request, "inventory/product_upload.html",{"form":form})
+
+def products_list(request):
+    products = Product.objects.all()
+    return render(request, "inventory/products_list.html", {"products": products})
+
+def product_detail_view(request):
+    product = Product.objects.get(id=id)
+    return render(request, "inventory/product_details.html", {"product": product})
+
+
   
